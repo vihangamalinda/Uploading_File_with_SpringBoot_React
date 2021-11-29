94f68c80a79c94b8e1a0a3cc1ff59d3265badc71
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2021-11-30 03:21:28 +0530

    Initializing the project.

---
+++ spring-boot/fileUploadSystem/src/main/java/com/springboot/fileUploadSystem/FileUploadSystemApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fileUploadSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadSystemApplication.class, args);
+	}
+
+}
---
+++ spring-boot/fileUploadSystem/src/test/java/com/springboot/fileUploadSystem/FileUploadSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.fileUploadSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
